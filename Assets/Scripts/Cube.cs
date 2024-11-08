0d60d47fb6bb5e74064afcdb203ad488d2f11b03
DESKTOP-0MBIPCQ\Andrey <drun.twin@mail.ru>
2024-11-08 18:49:53 +0300

    CubesPool. Задание выполнено!

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,34 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
     [SerializeField] private Renderer _renderer;
 
-    private bool _isColorChanged = false;
-    private int _minLifeTime = 2;
-    private int _maxLifeTime = 5;
+    private bool _isCollided = false;
+    private Color _defaultColor;
 
+    public delegate void CollideDelegate(GameObject gameObject);
+    public event CollideDelegate OnCollided;
+
+    private void Start()
+    {
+        _defaultColor = _renderer.material.color;
+    }
+
+    public void SetDefaultState()
+    {
+        _isCollided = false;
+        _renderer.material.color = _defaultColor;
+    }
 
     private void ChangeColor()
     {
         _renderer.material.color = Random.ColorHSV();
-        _isColorChanged = true;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (_isColorChanged == false)
+        if (_isCollided == false)
+        {
             ChangeColor();
-    }
-
-    private IEnumerator CreateCubes()
-    {
-        var waitSomeSeconds = new WaitForSecondsRealtime(Random.Range(_minLifeTime, _maxLifeTime));
-
-        yield return waitSomeSeconds;
-
-        
+            OnCollided?.Invoke(gameObject);
+            _isCollided = true;
+        }
     }
 }
--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -1,29 +1,98 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class CubeSpawner : MonoBehaviour
 {
-    [SerializeField] private Cube _cubePrefab;
+    [SerializeField] private GameObject _cubePrefab;
+    [SerializeField] private Transform _spawnArea;
+    [SerializeField] private float _repeatRate = 1f;
+    [SerializeField] private int _poolCapasity = 5;
+    [SerializeField] private int _poolMaxSize = 5;
+
+    private int _minLifeTime = 2;
+    private int _maxLifeTime = 5;
+    private ObjectPool<GameObject> _pool;
+
+    private void Awake()
+    {
+        _pool = new ObjectPool<GameObject>
+            (
+            createFunc: () => CreateFunc(),
+            actionOnGet: (obj) => ActionOnGet(obj),
+            actionOnRelease: (obj) => obj.SetActive(false),
+            actionOnDestroy: (obj) => DestroyCube(obj),
+            collectionCheck: true,
+            defaultCapacity: _poolCapasity,
+            maxSize: _poolMaxSize
+            );
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(GetCube), 0.0f, _repeatRate);
+    }
+
+    private GameObject CreateFunc()
+    {
+        GameObject obj = Instantiate(_cubePrefab);
+        obj.TryGetComponent(out Cube cube);
+        cube.OnCollided += ReleaseCube;
+
+        return obj;
+    }
+
+    private void GetCube()
+    {
+        _pool.Get();
+    }
+
+    private void ActionOnGet(GameObject obj)
+    {
+        int areaDivider = 2;
+
+        _spawnArea.TryGetComponent(out MeshRenderer spawnAreaRenderer);
+        Vector3 spawnAreaSize = spawnAreaRenderer.bounds.size;
+        float minXPos = _spawnArea.transform.position.x - spawnAreaSize.x / areaDivider;
+        float maxXPos = _spawnArea.transform.position.x + spawnAreaSize.x / areaDivider;
+        Vector3 startPosition = new Vector3(Random.Range(minXPos, maxXPos),
+                                            _spawnArea.transform.position.y,
+                                            _spawnArea.transform.position.z);
+
+        obj.transform.position = startPosition;
+        obj.TryGetComponent(out Rigidbody rigidbody);
+        rigidbody.velocity = Vector3.zero;
+        obj.SetActive(true);
+    }
 
 
-    //private IEnumerator CreateCubes()
-    //{
-    //    int spawnDelay;
-    //    var waitSomeSeconds = new WaitForSecondsRealtime(Random.Range(_minSpawnDelay, _maxSpawnDelay));
+    private void ReleaseCube(GameObject obj)
+    {
+        if(_pool.CountInactive > _poolMaxSize)
+        {
+            DestroyCube(obj);
+        }
+        else
+        {
+            StartCoroutine(DelayedDisappear(obj));
+        }
+    }
 
-    //    yield return waitSomeSeconds;
+    private void DestroyCube(GameObject obj)
+    {
+        obj.TryGetComponent(out Cube cube);
+        cube.OnCollided -= ReleaseCube;
+        Destroy(obj);
+    }
 
-    //    for (int i = 0; i < _diamondsMaxCount; i++)
-    //    {
-    //        float spawnPositionX = Random.Range(_spawnMinPositionX, _spawnMaxPositionX);
-    //        float spawnPositionZ = Random.Range(_spawnMinPositionZ, _spawnMaxPositionZ);
-    //        int spawnPositionY = 0;
+    IEnumerator DelayedDisappear(GameObject obj)
+    {
+        float randomLifeTime = Random.Range(_minLifeTime, _maxLifeTime);
 
-    //        Diamond diamond = Instantiate(_diamondPrefab, new Vector3(spawnPositionX, spawnPositionY, spawnPositionZ), _diamondPrefab.transform.rotation, transform);
-    //        _diamonds.Enqueue(diamond);
+        yield return new WaitForSeconds(randomLifeTime);
 
-    //        yield return waitSomeSeconds;
-    //    }
-    //}
+        obj.TryGetComponent(out Cube cube);
+        cube.SetDefaultState();
+        _pool.Release(obj);
+    }
 }
